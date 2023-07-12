9237eeebbd787567d82d264692491e79b546e89b
lfcastillo2016 <castillofelipe@gmail.com>
2023-07-12 11:41:46 +0000

    clase 12072023

--- srv/orders.js
+++ srv/orders.js
@@ -1,12 +1,23 @@
 const cds = require("@sap/cds");
 const { Orders } = cds.entities("com.training");
 
+//Sin filtros
+// module.exports = (srv) => {
 
+//     //*************READ **********/
+//     srv.on("READ", "GetOrders", async (req) => {
+//         return await SELECT.from(Orders);
+//     });
+
+// };
+
+//Con filtros
 module.exports = (srv) => {
-
-    //*************READ **********/
     srv.on("READ", "GetOrders", async (req) => {
+        if (req.data.ClientEmail !== undefined) {
+            return await SELECT.from`com.training.Orders`
+                .where`ClientEmail = ${req.data.ClientEmail}`;
+        }
         return await SELECT.from(Orders);
     });
-
 };
