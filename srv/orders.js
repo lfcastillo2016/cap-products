3a5cac8db8cc2c7f3b772c43fbb56074dac3d55c
lfcastillo2016 <castillofelipe@gmail.com>
2023-07-13 12:11:15 +0000

    clase 13072023

--- srv/orders.js
+++ srv/orders.js
@@ -13,11 +13,45 @@ const { Orders } = cds.entities("com.training");
 
 //Con filtros
 module.exports = (srv) => {
-    srv.on("READ", "GetOrders", async (req) => {
+    srv.on("READ", "Orders", async (req) => {
         if (req.data.ClientEmail !== undefined) {
             return await SELECT.from`com.training.Orders`
                 .where`ClientEmail = ${req.data.ClientEmail}`;
         }
         return await SELECT.from(Orders);
+    });
+
+    srv.after("READ", "Orders", (data) => {
+        return data.map((order) => (order.Reviewed = true));
+    });
+
+    srv.on("CREATE", "Orders", async (req) => {
+        let returnData = await cds
+            .transaction(req)
+            .run(
+                INSERT.into(Orders).entries({
+                    ClientEmail: req.data.ClientEmail,
+                    FirstName: req.data.FirstName,
+                    LastName: req.data.LastName,
+                    CreatedOn: req.data.CreatedOn,
+                    Reviewed: req.data.Reviewed,
+                    Approved: req.data.Approved,
+                })
+            )
+            .then((resolve, reject) => {
+                console.log("Resolve", resolve);
+                console.log("Reject", reject);
+                if (typeof resolve !== "undefined") {
+                    return req.data;
+                } else {
+                    req.error(409, "Record Not Inserted");
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                req.error(err.code, err.message);
+            });
+        console.log("Before End", returnData);
+        return returnData;
     });
 };
