445773120f80ff468aa98fd9317d72f37c154836
Luis Felipe Castillo <luisfelipe.castillo@globant.com>
2023-09-05 11:41:18 +0000

    clase 05092023

--- srv/server.js
+++ srv/server.js
@@ -4,7 +4,10 @@ const cors = require("cors"); // se hace uso del cors para realizar peticiones d
 // donde tenemos el servidor de aplicaciones levantado, para ver los datos de una entidad externa
 // a traves del servidor CAP
 
+const adapterProxy = require("products/srv/sapbackend-exit.cds");
+
 cds.on("bootstrap", (app) => {
+    app.use(adapterProxy());
     app.use(cors());
     app.get("/alive", (_res) => {
         res.status(200).send("Server is Alive")
